c3a538402494d1fe50b517d9ced2443313dd62b1
Connor <connorskorburg@gmail.com>
2020-10-15 15:43:48 -0500

    refactored functions and added comments

--- app/views.py
+++ app/views.py
@@ -7,13 +7,17 @@ import bcrypt
 from django.contrib import messages
 # Create your views here.
 
+
+
 # render home page
 def index(request):
   return render(request, 'index.html')
 
 
+
 # process login
 def login(request):
+  # check for valid login
   errors = validate_login(request.POST)
   if len(errors) > 0:
     for key, val in errors.items():
@@ -21,14 +25,17 @@ def login(request):
       messages.error(request, val)
     return redirect('/')
   else:
+    # fetch user
     mysql = MySQLConnection('MyTradeDB')
     query = 'SELECT * FROM user WHERE username = %(username)s;'
     data = {
       'username': request.POST['username']
     }
     users = mysql.query_db(query, data)
+    # check for valid user
     if len(users) > 0:
       user = users[0]
+      # check if password matches user and return user
       if bcrypt.checkpw(request.POST['password'].encode(), user['password'].encode()):
         request.session['user_id'] = user['id']
         return redirect('/dashboard')
@@ -38,23 +45,30 @@ def login(request):
     else:
       return redirect('/')
 
+
+
 # process logout
 def logout(request):
   if not 'user_id' in request.session:
     return redirect('/')
   else:
+    # clear session
     request.session.flush()
     return redirect('/')
 
 
+
 # process register
 def register(request):
+  # check for valid input
   errors = validate_registration(request.POST)
   if len(errors) > 0:
     for key, val in errors.items():
       messages.error(request, val)
     return redirect('/')
+  # check to see if passwords match
   elif request.POST['password'] == request.POST['conf_password']:
+    # hash and salt password
     password = request.POST['password']
     password_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
     # create the user
@@ -74,11 +88,13 @@ def register(request):
     return redirect('/')
 
 
+
 # render dashboard
 def dashboard(request):
   if not 'user_id' in request.session:
     return redirect('/')
   else:
+    # fetch user
     mysql = MySQLConnection('MyTradeDB')
     query = 'SELECT * FROM user WHERE id = %(id)s'
     data = {
@@ -86,7 +102,6 @@ def dashboard(request):
     }
     users = mysql.query_db(query, data)
     user = users[0]
-    print(user)
     # get latest trades
     mysql = MySQLConnection('MyTradeDB')
     trades_query = 'SELECT * FROM trade WHERE user_id = %(id)s ORDER BY created_at DESC LIMIT 10;'
@@ -94,12 +109,13 @@ def dashboard(request):
       'id': request.session['user_id']
     }
     trades = mysql.query_db(trades_query, trades_data)
+    # check for valid trade
     if len(trades) == 0:
       trades = False
       latest_trade = False
     else:
       latest_trade = trades[0]
-    print(trades)
+    # fetch latest news
     key = os.environ.get('FINHUB_API_KEY')
     r = requests.get(f'https://finnhub.io/api/v1/news?category=general&token={key}')
     res = r.json()
@@ -112,6 +128,7 @@ def dashboard(request):
       'trades': trades,
     }
     return render(request, 'dashboard.html', context)
+
 
 
 # render search page
@@ -131,7 +148,6 @@ def showSearch(request):
     }
     users = mysql.query_db(query, data)
     user = users[0]
-    print(user)
     context = {
       'results': json.dumps(r.json()),
       'current': "{:.2f}".format(res['c']),
@@ -141,16 +157,19 @@ def showSearch(request):
     return render(request, 'search.html', context)
 
 
+
 def search(request):
   if not 'user_id' in request.session:
     return redirect('/')
   else:
-    print(request.POST)
+    # check for valid stock symbol
     if request.POST['symbol'] == '':
       return redirect('/dashboard')
     else:
+      # pass symbol data to search query route
       request.session['symbol'] = request.POST['symbol']
       return redirect('/showSearch')
+
 
 
 # buy trade
@@ -177,7 +196,6 @@ def buyTrade(request):
       'user_id': request.session['user_id']
     }
     result = mysql.query_db(query, data)
-    print(result)
     # # fetch user from DB
     mysql = MySQLConnection('MyTradeDB')
     user_query = 'SELECT * FROM user WHERE id = %(id)s'
@@ -199,13 +217,11 @@ def buyTrade(request):
 
 
 
-
 # render search page
 def sellTrade(request):
   if not 'user_id' in request.session:
     return redirect('/')
   else:
-
 
     # get user 
     mysql = MySQLConnection('MyTradeDB')
@@ -215,12 +231,9 @@ def sellTrade(request):
     }
     users = mysql.query_db(user_query, user_data)
     user = users[0]
-    print("USER", user)
-
 
     # update user balance
     new_balance = float(user['account_balance']) + float(request.POST['total_price_gained'])
-    print(new_balance)
     mysql = MySQLConnection('MyTradeDB')
     update_user_query = 'UPDATE user SET account_balance = %(balance)s WHERE id = %(user_id)s;'
     update_user_data = {
@@ -228,7 +241,6 @@ def sellTrade(request):
       'user_id': request.session['user_id'],
     }
     updated_user = mysql.query_db(update_user_query, update_user_data)
-
 
     # get trade bought
     mysql = MySQLConnection('MyTradeDB')
@@ -241,14 +253,10 @@ def sellTrade(request):
       trade = False
     else: 
       trade = trades[0]
-    print("TRADE",trade)
-
 
     # updated trade bought
     updated_shares = int(trade['shares']) - int(request.POST['shares_sold'])
     updated_total_price = float(trade['total_price']) - float(request.POST['total_price_gained'])
-    print("UPDATED SHARES:", updated_shares)
-    print("UPDATED TOTAL:", updated_total_price)
     mysql = MySQLConnection('MyTradeDB')
     updated_trade_query = 'UPDATE trade SET shares = %(updated_shares)s, total_price = %(updated_total_price)s WHERE id = %(trade_id)s;'
     updated_trade_data = {
@@ -257,7 +265,6 @@ def sellTrade(request):
       'trade_id': request.POST['trade_id']
     }
     updated_trade = mysql.query_db(updated_trade_query, updated_trade_data)
-
 
     # create sold trade
     mysql = MySQLConnection('MyTradeDB')
@@ -271,15 +278,7 @@ def sellTrade(request):
       'trade_id': request.POST['trade_id'],
     }
     sold_trade = mysql.query_db(sold_query, sold_data)
-    print('SYMBOL', request.POST['symbol'])
-    print("Trade ID",request.POST['trade_id'])
-    print("Price Sold",request.POST['price_sold'])
-    print("Shares Sold",request.POST['shares_sold'])
-    print("Total Gained",request.POST['total_price_gained'])
     return redirect('/trades')
-
-
-
 
 
 
@@ -288,6 +287,7 @@ def sell(request, id):
   if not 'user_id' in request.session:
     return redirect('/')
   else:
+    # fetch user
     mysql = MySQLConnection('MyTradeDB')
     user_query = 'SELECT * FROM user WHERE id = %(id)s'
     user_data = {
@@ -295,19 +295,20 @@ def sell(request, id):
     }
     users = mysql.query_db(user_query, user_data)
     user = users[0]
-    user_query = 'SELECT * FROM'
+    # fetch trade to sell
     mysql = MySQLConnection('MyTradeDB')
     query = 'SELECT * FROM trade WHERE id = %(id)s;'
     data = {
       'id': id,
     }
     trade = mysql.query_db(query, data)
+    # check to see if trade is found
     if len(trade) == 0:
       trade = False
       current = False
     else:
       trade = trade[0]
-      print(trade)
+      # fetch current price
       key = os.environ.get('FINHUB_API_KEY')
       symbol = trade['symbol']
       r = requests.get(f'https://finnhub.io/api/v1/quote?symbol={symbol}&token={key}')
@@ -321,11 +322,13 @@ def sell(request, id):
     return render(request, 'sell.html', context)
 
 
+
 # render sold trades 
 def trades(request):
   if not 'user_id' in request.session:
     return redirect('/')
   else:
+    # fetch user
     mysql = MySQLConnection('MyTradeDB')
     query = 'SELECT * FROM user WHERE id = %(id)s'
     data = {
@@ -340,14 +343,17 @@ def trades(request):
       'id': request.session['user_id'],
     }
     trades = mysql.query_db(trades_query, trades_data)
+    # check to see if valid trade
     if len(trades) == 0:
       trades = False
+    # find sold trades
     mysql = MySQLConnection('MyTradeDB')
     sold_query = 'SELECT * FROM sold_trade WHERE user_id = %(id)s ORDER BY created_at DESC LIMIT 10;'
     sold_data = {
       'id': request.session['user_id'],
     }
     sold_trades = mysql.query_db(sold_query, sold_data)
+    # check if sold trades
     if len(sold_trades) == 0:
       sold_trades = False
     context = {
@@ -357,19 +363,23 @@ def trades(request):
     }
     return render(request, 'trades.html', context)
 
+
+
 # add balance
 def add_balance(request):
   if not 'user_id' in request.session:
     return redirect('/')
   else:
+    # check for valid input
     if request.POST['amount'] == '' or float(request.POST['amount']) <= 0:
       messages.error(request, 'Please Enter Amount!')
       return redirect('/trades')
+    # check for valid amount
     elif float(request.POST['amount']) > 1000:
       messages.error(request, 'Amount Must Be $1000 or less!')
       return redirect('/trades')
     else:
-      print('success')
+      # fetch user
       mysql = MySQLConnection('MyTradeDB')
       query = 'SELECT * FROM user WHERE id = %(id)s'
       data = {
@@ -378,6 +388,7 @@ def add_balance(request):
       users = mysql.query_db(query, data)
       user = users[0]
       new_balance = float(request.POST['amount']) + float(user['account_balance'])
+      # update user with new balance
       mysql = MySQLConnection('MyTradeDB')
       balance_query = 'UPDATE user SET account_balance = %(balance)s WHERE id = %(id)s;'
       balance_data = {
@@ -385,6 +396,5 @@ def add_balance(request):
         'id': request.session['user_id'],
       }
       updated_user = mysql.query_db(balance_query, balance_data)
-      print(updated_user)
       return redirect('/trades')
   return redirect('/')
